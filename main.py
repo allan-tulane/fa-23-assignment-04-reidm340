02ff5cac57c961b777122cf5fad18502c14e580e
Reid Miller <rmiller14@tulane.edu>
2023-11-10 13:43:03 -0600

    maybefinal

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relev
 alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
+    # TO DO - modify to account for inss, deletions and substitutions
     if (S == ""):
-        return(len(T))
+        return len(T)
+    
     elif (T == ""):
-        return(len(S))
+        return len(S)
+    
     else:
         if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
+            return MED(S[1:], T[1:])
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[:-1], T)))
+            return (1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[:-1], T)))
 
 
 def fast_MED(S, T, MED={}):
     if (S, T) in MED.keys():
         return MED[(S, T)]
+    
     else:
         if (S == ""):
             MED[(S, T)] = len(T)
@@ -32,11 +35,13 @@ def fast_MED(S, T, MED={}):
                 MED[(S, T)] = fast_MED(S[1:], T[1:], MED)
             else:
                 MED[(S, T)] = 1 + min(fast_MED(S, T[1:], MED), fast_MED(S[1:], T, MED), fast_MED(S[:-1], T, MED))
+    
     return MED[(S, T)]
 
 def fast_align_MED(S, T, MED={}):
     if (S, T) in MED.keys():
         return MED[(S, T)]
+    
     else:
         if (S == ""):
             MED[(S, T)] = (len(T), ('-' * len(T), T))
@@ -47,17 +52,16 @@ def fast_align_MED(S, T, MED={}):
                 temp = fast_align_MED(S[1:], T[1:], MED)
                 MED[(S, T)] = (temp[0], (S[0] + temp[1][0], T[0] + temp[1][1]))
             else:
-                
-                insertion = fast_align_MED(S, T[1:], MED)
-                deletion = fast_align_MED(S[1:], T, MED)
-                substitution = fast_align_MED(S[:-1], T, MED)
+                ins = fast_align_MED(S, T[1:], MED)
+                rem = fast_align_MED(S[1:], T, MED)
+                sub = fast_align_MED(S[:-1], T, MED)
 
-                if insertion[0] <= deletion[0] and insertion[0] <= substitution[0]:
-                    MED[(S, T)] = (1 + insertion[0], ('-' + insertion[1][0], T[0] + insertion[1][1]))
-                elif deletion[0] <= insertion[0] and deletion[0] <= substitution[0]:
-                    MED[(S, T)] = (1 + deletion[0], (S[0] + deletion[1][0], '-' + deletion[1][1]))
+                if ins[0] <= rem[0] and ins[0] <= sub[0]:
+                    MED[(S, T)] = (1 + ins[0], ('-' + ins[1][0], T[0] + ins[1][1]))
+                elif rem[0] <= ins[0] and rem[0] <= sub[0]:
+                    MED[(S, T)] = (1 + rem[0], (S[0] + rem[1][0], '-' + rem[1][1]))
                 else:
-                    MED[(S, T)] = (2 + substitution[0], (S[0] + substitution[1][0], T[0] + substitution[1][1]))
+                    MED[(S, T)] = (2 + sub[0], (S[0] + sub[1][0], T[0] + sub[1][1]))
         
         return MED[(S, T)]
 
